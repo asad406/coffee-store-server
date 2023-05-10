3ff65d459f26094cadaccd2c9f15e42c6f2949fc
asad406 <asadl76@gmail.com>
2023-05-10 09:18:23 +0600

    dotenv install and file added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASS)
+console.log(process.env.DB_PASS)
 //middleware
 app.use(cors());
 app.use(express.json());
